268f6d1a44a0061314f1a656999f8ad72902bfb4
Akevilli <dzmitry.nikifarau@innowise.com>
2025-10-14 13:52:31 +0300

    DS-2.1 Baseline modeling Changes: - clusters exploration - Baseline - SARIMA pipeline - Gradient boosting pipeline - Transformations for pipelines

--- Pipeline.py
+++ Pipeline.py
@@ -1,71 +1,13 @@
-from typing import Union
+from typing import Callable
 
 import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class Pipeline:
-    def __init__(self, steps: list[tuple[str, Union[BaseEstimator, TransformerMixin], bool]]):
-        """
-        Initializes the pipeline with a list of steps and a separate model.
-
-        Args:
-            steps (list[tuple[str, object, bool]]): A list of tuples, where each tuple
-                contains a step's name, the step object (transformer), and a boolean
-                flag to skip it during prediction.
-            model: The final model to be trained and used for prediction.
-        """
-
-        self.steps = {step: [action, skip] for step, action, skip in steps}
-
-    def fit(self, X: pd.DataFrame, y: pd.Series):
-        """
-        Fits the pipeline's steps and the final model.
-
-        Args:
-            X (pd.DataFrame): The training data.
-            y (pd.Series): The target labels.
-        """
-        X_processed = X.copy()
-        for name, (step, _) in self.steps.items():
-            if hasattr(step, 'fit_transform'):
-                X_processed = step.fit_transform(X_processed, y)
-            elif hasattr(step, 'fit') and hasattr(step, 'transform'):
-                step.fit(X_processed, y)
-                X_processed = step.transform(X_processed)
-            elif hasattr(step, 'transform'):
-                X_processed = step.transform(X_processed)
-            else:
-                raise AttributeError(f"Step '{name}' has no valid fit/transform methods.")
-
-    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Applies transformations and uses the trained model to make predictions.
-
-        Args:
-            X (pd.DataFrame): The data to make predictions on.
-            start (int, optional): The index of the first transformation step. Defaults to 0.
-            end (int, optional): The index of the last transformation step. Defaults to -1.
-        """
-        X_processed = X.copy()
-        for name, (step, skip) in self.steps.items():
-            if skip:
-                continue
-
-            if hasattr(step, 'transform'):
-                X_processed = step.transform(X_processed)
-            else:
-                raise AttributeError(f"Step '{name}' has no 'transform' method.")
-
-        return X_processed
-
-
 class GroupAndSummarize(BaseEstimator, TransformerMixin):
     """
     A custom transformer to group data, summarize columns, and add new ones.
-
-    This class is designed to be a step in a scikit-learn compatible pipeline.
     """
 
     def __init__(self, group_by_col: list[str], agg_dict: dict):
@@ -153,50 +95,6 @@ class DateDivider(BaseEstimator, TransformerMixin):
         return df
 
 
-class LagFeatureGenerator(BaseEstimator, TransformerMixin):
-    """
-    A transformer that generates lag features for a time series.
-
-    This class creates new columns based on past values of a specified column,
-    grouped by specified key columns. It's crucial for providing a model with
-    historical context in time series forecasting.
-    """
-    def __init__(self, column: str, lags: list[int], group_by_col: list[str], sort_by_col: list[str]):
-        """
-        Initializes the transformer.
-
-        Args:
-            column (str): The column for which to generate lag features.
-            lags (list[int]): A list of integer lags (e.g., [1, 2, 7]) to create.
-            group_by_col (list[str]): The columns to group by before shifting.
-            sort_by_col (list[str]): The columns to sort by to ensure correct lag order.
-        """
-        self.column = column
-        self.lags = lags
-        self.group_by_col = group_by_col
-        self.sort_by_col = sort_by_col
-
-    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
-        """
-        Generates lag features for the specified column.
-
-        The DataFrame is first sorted, then grouped, and finally the `shift`
-        method is used to create lagged versions of the target column.
-
-        Args:
-            X (pd.DataFrame): The input DataFrame.
-
-        Returns:
-            pd.DataFrame: The DataFrame with new lag feature columns.
-        """
-        df = X.copy()
-        df = df.sort_values(self.sort_by_col)
-        for lag in self.lags:
-            df[f"lag_{lag}"] = df.groupby(by=self.group_by_col)[self.column].shift(lag)
-
-        return df
-
-
 class NanDropper(BaseEstimator, TransformerMixin):
     """
     A transformer that drops rows with missing values.
@@ -213,6 +111,12 @@ class NanDropper(BaseEstimator, TransformerMixin):
                 If None, rows with NaNs in any column are dropped.
         """
         self.columns = columns
+
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        The fit method for transformers. In this case, it doesn't learn anything.
+        """
+        return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         """
@@ -235,6 +139,8 @@ class NanDropper(BaseEstimator, TransformerMixin):
 class LogTransformer(BaseEstimator, TransformerMixin):
     """
     A transformer that applies logarithmic transformation.
+
+    Calculates log function for selected columns, it helps to handle highly right-skewed data
     """
     def __init__(self, columns: list[str], add_one: bool = False):
         """
@@ -246,6 +152,12 @@ class LogTransformer(BaseEstimator, TransformerMixin):
         """
         self.columns = columns
         self.add_one = add_one
+
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        The fit method for transformers. In this case, it doesn't learn anything.
+        """
+        return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         """
@@ -270,6 +182,8 @@ class LogTransformer(BaseEstimator, TransformerMixin):
 class MovingAverageGenerator(BaseEstimator, TransformerMixin):
     """
     A transformer that applies moving average transformation.
+
+    Calculate rolling mean based on previous values.
     """
     def __init__(self, column: str, window_size: int, group_by_col: list[str], sort_by_col: list[str], min_periods: int):
         """
@@ -287,6 +201,12 @@ class MovingAverageGenerator(BaseEstimator, TransformerMixin):
         self.group_by_col = group_by_col
         self.sort_by_col = sort_by_col
         self.min_periods = min_periods
+
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        The fit method for transformers. In this case, it doesn't learn anything.
+        """
+        return self
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         """
@@ -308,4 +228,108 @@ class MovingAverageGenerator(BaseEstimator, TransformerMixin):
 
         df[f"MA_{self.column}_{self.window_size}_lags"] = data
 
+        return df
+
+
+class DifferenceGenerator(BaseEstimator, TransformerMixin):
+    """
+    A transformer that applies N-order difference transformation (Y_t - Y_t-lag)
+    for grouped time series data.
+    """
+
+    def __init__(
+            self,
+            columns: list[str],
+            lag: int = 1,
+            count: int = 1,
+            group_by_col: list[str] = None,
+            sort_by_col: list[str] = None
+    ):
+        """
+        Initializes the transformer.
+
+        Args:
+            columns (list[str]): A list of columns to generate differences.
+            lag (int, optional): The step of differencing. Defaults to 1.
+            count (int, optional): The number of differences. Defaults to 1.
+            group_by_col (list[str], optional): The columns to group by before differencing. Defaults to None.
+            sort_by_col (list[str], optional): The columns to sort by to ensure correct order. Defaults to None.
+        """
+        self.columns = columns
+        self.lag = lag
+        self.count = count
+        self.group_by_col = group_by_col
+        self.sort_by_col = sort_by_col
+
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        The fit method for transformers. In this case, it doesn't learn anything.
+        """
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Applies difference transformation using the transform method for correct alignment.
+
+        Args:
+            X (pd.DataFrame): The input DataFrame.
+        Returns:
+            pd.DataFrame: The transformed DataFrame.
+        """
+        df: pd.DataFrame = X.copy()
+
+        if self.sort_by_col:
+            df = df.sort_values(self.sort_by_col)
+
+        for column in self.columns:
+
+            def apply_n_diff(series):
+                result = series
+                for _ in range(self.count):
+                    result = result.diff(self.lag)
+                return result
+
+            if self.group_by_col:
+                diff_series = df.groupby(self.group_by_col)[column].transform(apply_n_diff)
+            else:
+                diff_series = apply_n_diff(df[column])
+
+            df[f"{column}_diff_lag{self.lag}_c{self.count}"] = diff_series
+
+        return df
+
+
+class ConditionDropper(BaseEstimator, TransformerMixin):
+    """
+    A transformer that drops rows by condition.
+    """
+    def __init__(self, column: str, condition: Callable):
+        """
+        Initializes the transformer.
+
+        Args:
+            column (str): The column to check for condition.
+            condition (Callable): The condition to check.
+        """
+        self.column = column
+        self.condition = condition
+
+    def fit(self, X: pd.DataFrame, y=None):
+        """
+        The fit method for transformers. In this case, it doesn't learn anything.
+        """
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """
+        Drops rows by passed condition.
+
+        Args:
+            X (pd.DataFrame): The input DataFrame.
+        Returns:
+            pd.DataFrame: The transformed DataFrame.
+        """
+        df: pd.DataFrame = X.copy()
+
+        df = df[self.condition(df[self.column])]
         return df
